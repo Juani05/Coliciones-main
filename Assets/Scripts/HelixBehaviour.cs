657d1773f7082a5cf97a84b3d627185bffb7cb13
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-05-31 21:07:13 -0300

    Initial commit

---
+++ Assets/Scripts/HelixBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixBehaviour : MonoBehaviour
+{
+    public float rotationspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, rotationspeed, 0);
+    }
+}
---
+++ Assets/Scripts/playsound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playsound : MonoBehaviour
+{
+    public AudioClip playersound;
+    public AudioClip playerdied;
+    AudioSource fuenteaudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuenteaudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            fuenteaudio.clip = playersound;
+            fuenteaudio.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            fuenteaudio.clip = playersound;
+            fuenteaudio.Play();
+        }
+    }
+}
